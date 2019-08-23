1ac0036120f18de75736f9bd0e1407c33236f077
Jakub Bujny <jakub.bujny@j-labs.pl>
2019-08-23 17:06:20 +0200

    improve

--- k8s/compass/build-and-push-image.sh
+++ k8s/compass/build-and-push-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t digitalrasta/compass:aws-07 .
+docker build -t digitalrasta/compass:aws-11 .
 
-docker push digitalrasta/compass:aws-07
+docker push digitalrasta/compass:aws-11
--- k8s/compass/entrypoint.sh
+++ k8s/compass/entrypoint.sh
@@ -7,10 +7,15 @@ mkdir -p /layers
 cd /layers
 aws s3 cp --recursive s3://iota-solution-s3-layers .
 
-ls -lah /data | wc -l
 
 cd /
-if [[ $(ls -lah /data | wc -l) -gt 4 ]]; then
+
+
+rm -r /data/lost+found
+
+ls -lah /data
+
+if [[ $(ls -lah /data | wc -l) -gt 3 ]]; then
     java -jar /coordinator_deploy.jar -layers /layers -statePath /data/compass.state  -sigMode CURLP27 -powMode CURLP81 -mwm 7 -security 1 -seed AITTDKCFUO9UPAFEAUUHYV9Y9LHGAZ9HMMFSDNVHQGFGIUEADK9RMGSZXXOFN9XEODJBCNK9EFZNETBRC -tick 90000 -host http://iri-03:14265 -broadcast
 else
     java -jar /coordinator_deploy.jar -layers /layers -statePath /data/compass.state  -sigMode CURLP27 -powMode CURLP81 -mwm 7 -security 1 -seed AITTDKCFUO9UPAFEAUUHYV9Y9LHGAZ9HMMFSDNVHQGFGIUEADK9RMGSZXXOFN9XEODJBCNK9EFZNETBRC -tick 90000 -host http://iri-03:14265 -broadcast -bootstrap
